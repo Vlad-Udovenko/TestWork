95594059162f1693965167f139fd7cafebaba91c
Udaff <udaffchik@gmail.com>
2020-05-13 18:04:32 +0200

    beautify code

--- js/index.js
+++ js/index.js
@@ -1,19 +1,25 @@
-function ValidPhone() {
+function ValidatePhone() {
+
     const re = /^\d[\d\(\)\ -]{4,14}\d$/;
     const phone = document.getElementById('phone').value;
-    const valid = re.test(phone);
-    if (valid) output = 'Номер телефона введен правильно!';
-    else output = 'Номер телефона введен неправильно!';
-    document.getElementById('message').innerHTML = document.getElementById('message').innerHTML+'<br />'+output;
-    
-    return false;
-}  
-function ValidName() {
-    const re =/^[A-Za-z]+$/;
+    const isvalid = re.test(phone);
+
+    isvalid ? output = 'Номер телефона введен правильно!' : output = 'Номер телефона введен неправильно!';
+    document.getElementById('message').innerHTML = document.getElementById('message').innerHTML + '<br />' + output;
+
+    return isvalid;
+
+}
+
+function ValidateName() {
+
+    const re = /^[A-Za-z]+$/;
     const name = document.getElementById('name').value;
-    const valid = re.test(name);
-    if (valid) output = 'Имя введено правильно!';
-    else output = 'Имя введено неправильно!';
+    const isvalid = re.test(name);
+
+    isvalid ? output = 'Имя введено правильно!': output = 'Имя введено неправильно!';
     document.getElementById('message').innerHTML = output;
-    return valid;
+
+    return isvalid;
+    
 }

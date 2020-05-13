15744d65ab9d397097086838b9aae4bc01f28490
Udaff <udaffchik@gmail.com>
2020-05-13 17:34:49 +0200

    small fix

--- js/index.js
+++ js/index.js
@@ -1,16 +1,17 @@
 function ValidPhone() {
-    let re = /^\d[\d\(\)\ -]{4,14}\d$/;
-    let phone = document.getElementById('phone').value;
-    let valid = re.test(phone);
+    const re = /^\d[\d\(\)\ -]{4,14}\d$/;
+    const phone = document.getElementById('phone').value;
+    const valid = re.test(phone);
     if (valid) output = 'Номер телефона введен правильно!';
     else output = 'Номер телефона введен неправильно!';
     document.getElementById('message').innerHTML = document.getElementById('message').innerHTML+'<br />'+output;
-    return valid;
+    
+    return false;
 }  
 function ValidName() {
-    let re =/^[A-Za-z]+$/;
-    let name = document.getElementById('name').value;
-    let valid = re.test(name);
+    const re =/^[A-Za-z]+$/;
+    const name = document.getElementById('name').value;
+    const valid = re.test(name);
     if (valid) output = 'Имя введено правильно!';
     else output = 'Имя введено неправильно!';
     document.getElementById('message').innerHTML = output;
